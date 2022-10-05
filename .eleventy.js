7771ed4f6bc32cb728273eda5c1e2c4361c44be5
Max Stubbersfield <max.stubbersfield@gmail.com>
2022-10-05 10:31:54 +0200

    Create pages for privacy policy, terms and conditions and imprint.

--- .eleventy.js
+++ .eleventy.js
@@ -1,35 +1,42 @@
 const { EleventyI18nPlugin } = require("@11ty/eleventy");
 const i18n = require('eleventy-plugin-i18n');
 const translations = require('./src/_data/i18n');
+const markdownIt = require("markdown-it");
 
-module.exports = function(eleventyConfig) {
-    // Plugins
-    eleventyConfig.addPlugin(EleventyI18nPlugin, {
-        // any valid BCP 47-compatible language tag is supported
-        defaultLanguage: "en",
-      });
+module.exports = function (eleventyConfig) {
+  // Plugins
+  eleventyConfig.addPlugin(EleventyI18nPlugin, {
+    // any valid BCP 47-compatible language tag is supported
+    defaultLanguage: "en",
+  });
 
-    eleventyConfig.addPlugin(i18n, {
-        translations
-      });
+  eleventyConfig.addPlugin(i18n, {
+    translations
+  });
 
-    eleventyConfig.setServerPassthroughCopyBehavior("copy");
+  const md = markdownIt();
 
-    // Pass through copy
-    eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPairedShortcode("markdown", (content) => {
+    return md.render(content);
+  });
 
-    // Watch
-    eleventyConfig.addWatchTarget("src/assets/");
+  eleventyConfig.setServerPassthroughCopyBehavior("copy");
 
-    return {
-      dir: {
-        input: 'src',
-        includes: '_includes',
-        output: '_site',
-      },
-      templateFormats: ['md', 'njk', 'html'],
-      markdownTemplateEngine: 'njk',
-      htmlTemplateEngine: 'njk',
-      dataTemplateEngine: 'njk',
-    };
-  }
+  // Pass through copy
+  eleventyConfig.addPassthroughCopy("src/assets/");
+
+  // Watch
+  eleventyConfig.addWatchTarget("src/assets/");
+
+  return {
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      output: '_site',
+    },
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+  };
+}
---
+++ src/assets/documents/imprint_de.md
@@ -0,0 +1,26 @@
+
+# Impressum
+
+
+###### Updated 14.12.2021
+
+## Legal Disclosure
+
+Information in accordance with Section 5 TMG
+
+## Address
+
+Me GbR‍  
+Mommsenstraße 5
+12203 Berlin  
+Germany
+
+## Managing Directors
+
+Yannick Schmid
+
+Max Stubbersfield
+
+## Contact
+
+knowyourself.meapp@gmail.com
