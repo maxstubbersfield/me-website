60e2f76d98b1bf39b48dd7379647ec242a3ded21
Max Stubbersfield <max.stubbersfield@gmail.com>
2022-10-06 16:19:28 +0200

    Update localizations.

--- src/_data/i18n/index.js
+++ src/_data/i18n/index.js
@@ -3,210 +3,208 @@ module.exports = {
         hero: {
             title: {
                 part1: {
-                    'en': 'Improve your',
-                    'de': 'Steigere deine'
+                    en: "Improve your",
+                    de: "Steigere deine"
                 },
                 part2: {
-                    'en': 'mental health',
-                    'de': 'psychische Gesundheit'
+                    en: "mental health",
+                    de: "psychische Gesundheit"
                 },
                 part3: {
-                    'en': 'sustainably',
-                    'de': 'nachhaltig'
+                    en: "sustainably",
+                    de: "nachhaltig"
                 }
             },
             subtitle: {
-                'en': 'Deep Self Reflection • Journaling • Mood Tracking',
-                'de': 'Tiefe Selbstreflexion • Tagebuchführen • Stimmungstracking'
+                en: "Deep Self Reflection • Journaling • Mood Tracking",
+                de: "Tiefe Selbstreflexion • Tagebuchführen • Stimmungstracking"
             },
             app_store_badge_alt: {
-                'en': 'App Store download button',
-                'de': 'Knopf zum laden im App Store'
+                en: "App Store download button",
+                de: "Knopf zum laden im App Store"
             },
             play_store_badge_alt: {
-                'en': 'Play Store download button',
-                'de': 'Knopf zum Herunterladen bei Google Play'
+                en: "Play Store download button",
+                de: "Knopf zum Herunterladen bei Google Play"
             },
             image_alt: {
-                'en': 'App screenshot showing Mindspace screen in the Me app.',
-                'de': ''
-            },
+                en: "App screenshot showing Mindspace screen in the Me app.",
+                de: ""
+            }
         },
         features: {
             slide_1: {
                 title: {
-                    'en': 'Why do I feel bad? And what can I do to get better?',
-                    'de': 'Warum fühle ich mich schlecht? Und was kann ich tun, um mich besser zu fühlen?'
+                    en: "Why do I feel bad? And what can I do to get better?",
+                    de: "Warum fühle ich mich schlecht? Und was kann ich tun, um mich besser zu fühlen?"
                 },
                 description: {
-                    'en': 'Identify the origin of your problems through regular self-reflection exercises',
-                    'de': 'Finde die Ursachen deiner Probleme durch regelmäßige Selbstreflexions-Übungen.'
+                    en: "Identify the origin of your problems through regular self-reflection exercises",
+                    de: "Finde die Ursachen deiner Probleme durch regelmäßige Selbstreflexions-Übungen."
                 },
                 image_alt: {
-                    'en': 'App screenshot showing graph of mood going up and down over time',
-                    'de': ''
-                },
+                    en: "App screenshot showing graph of mood going up and down over time",
+                    de: ""
+                }
             },
             slide_2: {
                 title: {
-                    'en': 'Different Problems -\nSame Solution',
-                    'de': 'Verschiedene Probleme -\n Gleiche Lösung'
+                    en: "Different Problems -\nSame Solution",
+                    de: "Verschiedene Probleme -\nGleiche Lösung"
                 },
                 description: {
-                    'en': 'Address your emotional challenges in different areas of your life with easy to understand concepts from psychotherapy.',
-                    'de': 'Bearbeite die emotionalen Herausforderungen in deinem Leben mit leicht verständlichen Methoden aus der Psychotherapie'
+                    en: "Address your emotional challenges in different areas of your life with easy to understand concepts from psychotherapy.",
+                    de: "Bearbeite die emotionalen Herausforderungen in deinem Leben mit leicht verständlichen Methoden aus der Psychotherapie"
                 },
                 image_alt: {
-                    'en': 'App screenshot showing selection list of ways in which the Me app can help them',
-                    'de': ''
-                },
+                    en: "App screenshot showing selection list of ways in which the Me app can help them",
+                    de: ""
+                }
             },
             slide_3: {
                 title: {
-                    'en': 'Your personal journal, always in your pocket',
-                    'de': 'Dein Tagebuch, immer in deiner Tasche'
+                    en: "Your personal journal, always in your pocket",
+                    de: "Dein Tagebuch, immer in deiner Tasche"
                 },
                 description: {
-                    'en': 'Build a habit of self-reflection to understand yourself better. Work with guided gratefulness exercises & affirmations to develop a more positive outlook on life.',
-                    'de': 'Mach Selbstreflexion zu einer Gewohnheit, um dich selbst besser zu verstehen. Arbeite mit geführten Dankbarkeitsübungen und Affirmationen um einen positiveren Blick auf das Leben zu gewinnen.'
+                    en: "Build a habit of self-reflection to understand yourself better. Work with guided gratefulness exercises & affirmations to develop a more positive outlook on life.",
+                    de: "Mach Selbstreflexion zu einer Gewohnheit, um dich selbst besser zu verstehen. Arbeite mit geführten Dankbarkeitsübungen und Affirmationen um einen positiveren Blick auf das Leben zu gewinnen."
                 },
                 image_alt: {
-                    'en': 'App screenshot showing list of journal entries',
-                    'de': ''
-                },
+                    en: "App screenshot showing list of journal entries",
+                    de: ""
+                }
             },
             slide_4: {
                 title: {
-                    'en': 'Dig deeper into your mind',
-                    'de': 'Erforsche die Tiefen deiner Psyche'
+                    en: "Dig deeper into your mind",
+                    de: "Erforsche die Tiefen deiner Psyche"
                 },
                 description: {
-                    'en': "It's worth it!\nExplore your unconscious thoughts & emotions to learn where your behavioral patterns come from and how you can change them.",
-                    'de': 'Es lohnt sich!\nErgründe deine unbewussten Emotionen & Gedanken, um zu lernen wo deine Verhaltensmuster herkommen und wie du sie ändern kannst.'
+                    en: "It's worth it!\nExplore your unconscious thoughts & emotions to learn where your behavioral patterns come from and how you can change them.",
+                    de: "Es lohnt sich!\nErgründe deine unbewussten Emotionen & Gedanken, um zu lernen wo deine Verhaltensmuster herkommen und wie du sie ändern kannst."
                 },
                 image_alt: {
-                    'en': 'App screenshot showing a journal entry with emotions, psychological needs, and narratives',
-                    'de': ''
-                },
-            },
+                    en: "App screenshot showing a journal entry with emotions, psychological needs, and narratives",
+                    de: ""
+                }
+            }
         },
         video: {
             title: {
-                'en': 'Everyone can benefit from self-reflection',
-                'de': 'Wir alle können von Selbstreflexion profitieren'
-            },
+                en: "Everyone can benefit from self-reflection",
+                de: "Wir alle können von Selbstreflexion profitieren"
+            }
         },
         security_and_privacy: {
             title: {
-                'en': 'Data Privacy & Security',
-                'de': 'Datenschutz & Privatsphäre'
+                en: "Data Privacy & Security",
+                de: "Datenschutz & Privatsphäre"
             },
             subtitle: {
-                'en': 'We will never sell, analyze or even see your data!',
-                'de': 'Wir werden deine Daten nie verkaufen, analysieren oder einsehen können'
+                en: "We will never sell, analyze or even see your data!",
+                de: "Wir werden deine Daten nie verkaufen, analysieren oder einsehen können"
             },
             point_1: {
                 title: {
-                    'en': 'Locally on your phone',
-                    'de': 'Lokale Datenspeicherung'
+                    en: "Locally on your phone",
+                    de: "Lokale Datenspeicherung"
                 },
                 description: {
-                    'en': 'No sensitive data gets sent to any server in the cloud. Your data is only stored locally on your phone.',
-                    'de': 'Sensitive Daten werden an keinen Server in der Cloud gesendet. Deine Daten werden ausschließlich auf deinem Smartphone gespeichert.'
+                    en: "No sensitive data gets sent to any server in the cloud. Your data is only stored locally on your phone.",
+                    de: "Sensitive Daten werden an keinen Server in der Cloud gesendet. Deine Daten werden ausschließlich auf deinem Smartphone gespeichert."
                 },
                 image_alt: {
-                    'en': 'Cloud with strike through it',
-                    'de': 'durchgestrichene Cloud'
-                },
+                    en: "Cloud with strike through it",
+                    de: "Durchgestrichene Cloud"
+                }
             },
             point_2: {
                 title: {
-                    'en': 'Encrypted',
-                    'de': 'Verschlüsselt'
+                    en: "Encrypted",
+                    de: "Verschlüsselt"
                 },
                 description: {
-                    'en': 'The data on your phone is encrypted and can only be unlocked with your credentials.',
-                    'de': 'Die Daten auf deinem Smartphone sind verschlüsselt und können nur von dir entschlüsselt werden.'
+                    en: "The data on your phone is encrypted and can only be unlocked with your credentials.",
+                    de: "Die Daten auf deinem Smartphone sind verschlüsselt und können nur von dir entschlüsselt werden."
                 },
                 image_alt: {
-                    'en': 'Lock',
-                    'de': 'Schloss'
-                },
+                    en: "Lock",
+                    de: "Schloss"
+                }
             },
             point_3: {
                 title: {
-                    'en': 'No Tracking',
-                    'de': 'Kein Tracking'
+                    en: "No Tracking",
+                    de: "Kein Tracking"
                 },
                 description: {
-                    'en': "We don't collect analytics data about your user behaviour to make sure that you are completely anonymous in our app.",
-                    'de': 'Wir sammeln keine Analysedaten über dein Nutzungsverhalten, um sicherzustellen, dass du in der App vollständig anonym bleibst.'
+                    en: "We don't collect analytics data about your user behaviour to make sure that you are completely anonymous in our app.",
+                    de: "Wir sammeln keine Analysedaten über dein Nutzungsverhalten, um sicherzustellen, dass du in der App vollständig anonym bleibst."
                 },
                 image_alt: {
-                    'en': 'Crosshair-like circle with strike',
-                    'de': 'Durchgestrichenes Schild'
-                },
-            },
+                    en: "Crosshair-like circle with strike",
+                    de: "Durchgestrichenes Schild"
+                }
+            }
         },
         partners: {
             title: {
-                'en': 'Our Partners',
-                'de': 'Unsere Partner'
+                en: "Our Partners",
+                de: "Unsere Partner"
             },
-        },
-    },
-    footer: {
-        partners: {
             exist: {
                 image_alt: {
-                    'en': 'Logos of the Federal Ministry for Economic Affairs and Climate Action, EXIST, European Social Fund for Germany, and European Union.',
-                    'de': 'Logos von Bundesministerium für Wirtschaft und Klimaschutz, Existenzgründungen aus der Wissenschaft, Europäischer Sozialfonds für Deutschland, und Europäische Union.'
-                },
+                    en: "Logos of the Federal Ministry for Economic Affairs and Climate Action, EXIST, European Social Fund for Germany, and European Union.",
+                    de: "Logos von Bundesministerium für Wirtschaft und Klimaschutz, Existenzgründungen aus der Wissenschaft, Europäischer Sozialfonds für Deutschland, und Europäische Union."
+                }
             },
             bia: {
                 image_alt: {
-                    'en': 'Berlin Innovation Agency - Future Health Accelerator logo',
-                    'de': 'Berlin Innovation Agency - Future Health Accelerator logo'
-                },
+                    en: "Berlin Innovation Agency - Future Health Accelerator logo",
+                    de: "Berlin Innovation Agency - Future Health Accelerator logo"
+                }
             },
             berlin_senate: {
                 image_alt: {
-                    'en': 'Berlin Senate logo',
-                    'de': 'Berliner Senat Logo'
-                },
-            },
-        },
+                    en: "Berlin Senate logo",
+                    de: "Berliner Senat Logo"
+                }
+            }
+        }
+    },
+    footer: {
         copyright: {
-            'en': 'Me GbR All right reserved.',
-            'de': 'Me GbR alle Rechte vorbehalten'
+            en: "Me GbR All right reserved.",
+            de: "Me GbR alle Rechte vorbehalten."
         },
         links: {
             website_privacy_policy: {
-                'en': 'Website Privacy Policy',
-                'de': 'Datenschutzerklärung Website'
+                en: "Website Privacy Policy",
+                de: "Datenschutzerklärung Website"
             },
             app_privacy_policy: {
-                'en': 'App Privacy Policy',
-                'de': 'Datenschutzerklärung App'
+                en: "App Privacy Policy",
+                de: "Datenschutzerklärung App"
             },
             terms_and_conditions: {
-                'en': 'Terms & Conditions',
-                'de': 'Allgemeine Geschäftsbedingungen'
+                en: "Terms & Condition",
+                de: "Allgemeine Geschäftsbedingungen"
             },
             imprint: {
-                'en': 'Imprint',
-                'de': 'Impressum'
+                en: "Imprint",
+                de: "Impressum"
             }
         }
     },
     error_404: {
         title: {
-            'en': 'Oops!',
-            'de': 'Hoppla!'
+            en: "Oops!",
+            de: "Hoppla!"
         },
         description: {
-            'en': 'The page you are looking for could not be found.',
-            'de': 'Die aufgerufene Seite wurde leider nicht gefunden.'
+            en: "The page you are looking for could not be found.",
+            de: "Die aufgerufene Seite wurde leider nicht gefunden."
         }
     }
 };
